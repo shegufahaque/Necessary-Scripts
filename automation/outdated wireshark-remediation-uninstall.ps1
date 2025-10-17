3bd2740d7e2ae9327f6a97619446f815e1977a82
Shegufa <hoquesegufa@gmail.com>
2025-10-17 08:00:31 -0700

    Update outdated wireshark-remediation-uninstall.ps1

--- automation/outdated wireshark-remediation-uninstall.ps1
+++ automation/outdated wireshark-remediation-uninstall.ps1
@@ -5,15 +5,7 @@
     Please test thoroughly in a non-production environment before deploying widely.
     Make sure to run as Administrator or with appropriate privileges.
 
-.NOTES
-    Author        : Josh Madakor
-    Date Created  : 2024-09-09
-    Last Modified : 2024-09-09
-    Version       : 1.0
-
 .TESTED ON
-    Date(s) Tested  : 2024-09-09
-    Tested By       : Josh Madakor
     Systems Tested  : Windows Server 2019 Datacenter, Build 1809
     PowerShell Ver. : 5.1.17763.6189
     Wireshark Ver.  : 2.2.1 (v2.2.1-0-ga6fbd27 from master-2.2)
